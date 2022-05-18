030585028d0172304b8447be0c1338dd73354b99
smilly <smilly.3d@gmail.com>
2022-05-18 13:59:05 -0300

    refactor: INSTALLED_APPs

--- q4_demo/settings.py
+++ q4_demo/settings.py
@@ -29,16 +29,29 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+
+]
+
+MY_APPS = [
+    'accounts',
+    'invoices',
+    'orders',
+    'products',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
