250d51ab045ded6c4becf0fa71e69d52a5935bb5
smilly <smilly.3d@gmail.com>
2022-05-18 13:58:40 -0300

    create: app products

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from uuid import uuid4
+from django.db import models
+
+class Products(models.Model):
+    product_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023, null=True)
+    unit_value = models.FloatField()
