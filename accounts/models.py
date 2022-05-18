f351822a858881148b83925060294afeb0b07fa9
smilly <smilly.3d@gmail.com>
2022-05-18 13:52:34 -0300

    refactor: Models Accounts for ref sheet users

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,8 @@ from uuid import uuid4
 from django.db import models
 
 class Accounts(models.Model):
-  user_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-  email = models.EmailField(unique=True)
-  birthdate = models.DateField(auto_now_add=True)
-  account_balance = models.FloatField(default=0)
-  password = models.CharField(max_length=255)
+    user_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField(auto_now_add=True)
+    account_balance = models.FloatField(default=0)
+    password = models.CharField(max_length=255)
