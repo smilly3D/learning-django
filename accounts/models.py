e4874d32b0420fc23018b49d663811395537f6d5
smilly <smilly.3d@gmail.com>
2022-05-19 20:54:25 -0300

    fix: change model for AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
 from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Accounts(models.Model):
+class Accounts(AbstractUser):
     user_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField(unique=True)
     birthdate = models.DateField(null=True)
+    username = models.CharField(max_length=150, null=True)
     account_balance = models.FloatField(default=0)
-    password = models.CharField(max_length=255)
+    # password = models.CharField(max_length=255)
+
+    USERNAME_FIELD: str = "email"
+    REQUIRED_FIELDS: list[str] = []
